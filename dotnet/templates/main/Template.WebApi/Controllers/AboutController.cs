b1aab5f58db14c35fbcc5feacb40adfa481957b8
tarasov_es <pavluchenk0@yandex.ru>
2023-02-02 13:45:07 +0400

    structure refactoring

--- dotnet/templates/main/Template.WebApi/Controllers/AboutController.cs
+++ dotnet/templates/main/Template.WebApi/Controllers/AboutController.cs
@@ -1,18 +1,19 @@
 [Route(Uris.About)]
-public class AboutController 
+public class AboutController
 {
     public IHostEnvironment Environment { get; }
-
-    public AboutController(IHostEnvironment environment) { 
+    
+    public AboutController(IHostEnvironment environment) {
         this.Environment = environment;
     }
 
     [HttpGet]
-    public About GetAbout() {
+    public About GetAbout()
+    {        
         return new(
-            Description: "Template webapi",
-            Version: this.GetType().Assembly.GetName().Version!.ToString(),
-            Environment: this.Environment.EnvironmentName
+            Description : "Template webapi",
+            Version : this.GetType().Assembly.GetName().Version!.ToString(),
+            Environment : this.Environment.EnvironmentName
         );
     }
 }
