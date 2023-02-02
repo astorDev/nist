1be3d4d2993ff2a0c6a76e6c70d7fb54cf8a4292
tarasov_es <pavluchenk0@yandex.ru>
2023-02-02 22:19:31 +0400

    alignment back fixed

--- dotnet/templates/main/Template.Protocol/About.cs
+++ dotnet/templates/main/Template.Protocol/About.cs
@@ -1,21 +1,18 @@
 namespace Template;
 
-public partial class Uris
-{
+public partial class Uris {
     public const string About = "about";
 }
 
 public record About(string Description, string Version, string Environment);
 
-public partial class Client
-{
+public partial class Client {
     public HttpClient Http { get; }
     public Client(HttpClient http) { this.Http = http; }
 
     public Task<About> GetAbout() => this.Http.GetAsync(Uris.About).Read<About>();
 }
 
-public partial class Errors
-{
-    public static Error Unknown = new(HttpStatusCode.InternalServerError, "Unknown");
+public partial class Errors {
+    public static readonly Error Unknown = new(HttpStatusCode.InternalServerError, "Unknown");
 }
