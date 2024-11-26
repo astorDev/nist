1219dc535efa40943a5a98b502653b7afc626843
Egor Tarasov <pavluchenk0@yandex.ru>
2024-11-26 16:09:20 +0400

    minist to .net 9 (#83)

---
+++ templates/dotnet/protocol/About.cs
@@ -0,0 +1,15 @@
+﻿namespace Template;
+
+public partial class Uris {
+    public const string About = "about";
+}
+
+public partial interface IClient {
+    Task<About> GetAbout();
+}
+
+public partial class Client {
+    public async Task<About> GetAbout() => await Get<About>(Uris.About);
+}
+
+public record About(string Description, string Version, string Environment);
