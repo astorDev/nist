12eef150395135353bfbff7f6b2b4549f6a63490
Egor Tarasov <pavluchenk0@yandex.ru>
2024-12-15 08:27:41 +0400

    spacex protocol and tests

--- client/playground/SpaceX.Protocol/Launches.cs
+++ client/playground/SpaceX.Protocol/Launches.cs
@@ -1,1 +1,19 @@
-public record LaunchV1(string Name, bool? Success);
+using Microsoft.Extensions.Logging;
+using Nist.Responses;
+
+namespace SpaceX;
+
+public record Launch(string Name, bool? Success);
+
+public class Uris {
+    public const string Launches = "launches";
+    public const string Latest = "latest";
+
+    public static string LatestLaunch => $"{Launches}/{Latest}";
+}
+
+public class Client(HttpClient http, ILogger<Client> logger) {
+    public async Task<Launch> GetLatestLaunch() => await GetAsync<Launch>(Uris.LatestLaunch);
+
+    public Task<T> GetAsync<T>(string uri) => http.GetAsync(uri).Read<T>(logger);
+}
--- client/playground/SpaceX.Tests/Test1.cs
+++ client/playground/SpaceX.Tests/Test1.cs
@@ -1,29 +1,65 @@
-﻿using Nist.Responses;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Nist.Responses;
 
 namespace SpaceX.Tests;
 
 [TestClass]
 public sealed class LaunchesShould
 {
+    readonly ILogger logger;
+    readonly Client client;
+    
+    public LaunchesShould()
+    {
+        var services = new ServiceCollection()
+            .AddLogging(l => l.AddSimpleConsole(c => c.SingleLine = true))
+            .AddHttpClient<Client>(cl => cl.BaseAddress = new("https://api.spacexdata.com/v4/")).Services
+            .BuildServiceProvider();
+
+        logger = services.GetRequiredService<ILogger<LaunchesShould>>();
+        client = services.GetRequiredService<Client>();
+    }
+    
     [TestMethod]
-    public async Task BeReadableAsRawString()
+    public async Task ReturnLatestAsRawString()
     {
         var http = new HttpClient {
             BaseAddress = new("https://api.spacexdata.com/v4/")
         };
 
-        var response = await http.GetAsync("launches");
+        var response = await http.GetAsync(Uris.LatestLaunch);
         Console.WriteLine(await response.Content.ReadAsStringAsync());
     }
     
     [TestMethod]
-    public async Task BeReadableAsLaunchObject()
+    public async Task ReturnLatestAsLaunchObject()
     {
         var http = new HttpClient {
             BaseAddress = new("https://api.spacexdata.com/v4/")
         };
 
-        var launches = await http.GetAsync("launches").Read<LaunchV1[]>();
-        Console.WriteLine(launches[0]);
+        var response = await http.GetAsync(Uris.LatestLaunch);
+        var launch = await response.Read<Launch>();
+        Console.WriteLine(launch);
+    }
+    
+    [TestMethod]
+    public async Task ReturnLatestWithLogging()
+    {
+        var http = new HttpClient {
+            BaseAddress = new("https://api.spacexdata.com/v4/")
+        };
+
+        var response = await http.GetAsync(Uris.LatestLaunch);
+        var launch = await response.Read<Launch>(logger);
+        Console.WriteLine(launch);
+    }
+    
+    [TestMethod]
+    public async Task ReturnLatestFromTypedClient()
+    {
+        var launch = await client.GetLatestLaunch();
+        Console.WriteLine(launch);
     }
 }
