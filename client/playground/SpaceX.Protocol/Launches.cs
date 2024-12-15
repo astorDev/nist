fae1b3348f12cd4275c6072e94c1ed96ca045271
Egor Tarasov <pavluchenk0@yandex.ru>
2024-12-15 11:57:17 +0400

    client 101 texting

--- client/playground/SpaceX.Protocol/Launches.cs
+++ client/playground/SpaceX.Protocol/Launches.cs
@@ -1,3 +1,4 @@
+using System.Net.Http.Json;
 using Microsoft.Extensions.Logging;
 using Nist.Responses;
 
@@ -16,4 +17,5 @@ public class Client(HttpClient http, ILogger<Client> logger) {
     public async Task<Launch> GetLatestLaunch() => await GetAsync<Launch>(Uris.LatestLaunch);
 
     public Task<T> GetAsync<T>(string uri) => http.GetAsync(uri).Read<T>(logger);
+    public Task<T> PostAsync<T>(string uri, object body) => http.PostAsJsonAsync(uri, body).Read<T>(logger);
 }
