a353ac8f271a3cac99c86075eaa021d77f118128
Egor Tarasov <vosarat1995+1@gmail.com>
2025-05-05 13:16:12 +0300

    include proper tostring (#153)

---
+++ queries/include/tests/ToQuery.cs
@@ -0,0 +1,22 @@
+﻿using Shouldly;
+
+namespace Nist.Queries.Include.Tests;
+
+[TestClass]
+public sealed class ToQuery
+{
+    [TestMethod]
+    public void Standard()
+    {
+        var query = new QueryWithInclude(
+            IncludeQueryParameter.Parse("total,items.name")
+        );
+
+        var uri = QueryUri.From("resource", query);
+        uri.ToString().ShouldBe("resource?include=total%2Citems.name");
+    }
+}
+
+public record QueryWithInclude(
+    IncludeQueryParameter Include
+);
