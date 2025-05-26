0ed3f559eb86cfb9bd24514d90f28c62192eec9a
Egor Tarasov <vosarat1995@google.com>
2025-05-26 13:53:23 +0400

    playground template used for webhooks/playground plus versions update

--- webhooks/playground/Versions/final/Program.cs
+++ webhooks/playground/Versions/final/Program.cs
@@ -5,7 +5,7 @@ builder.Logging.AddSimpleConsole(c => c.SingleLine = true);
 var app = builder.Build();
 
 app.MapGet("/", () => new {
-    Message = "Hello World!"
+    Message = "Hello Final!"
 });
 
 app.Run();
