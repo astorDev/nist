5fca94eccf7380906b09419e309691c5b8b0d280
Egor Tarasov <vosarat1995+1@gmail.com>
2025-10-23 13:04:15 +0400

    Nist.Queries.Sort Nuget (#170)

---
+++ queries/sort/lib/SortQueryParameter.cs
@@ -0,0 +1,37 @@
+﻿namespace Nist;
+
+public record SortQueryParameter(CommaSeparatedParameters<SortField> Inner) : CommaSeparatedParameters<SortField>(Inner)
+{
+    public static bool TryParse(string source, out SortQueryParameter includeQueryParameter)
+    {
+        includeQueryParameter = Parse(source);
+        return true;
+    }
+
+    public static SortQueryParameter Parse(string source)
+    {
+        return new SortQueryParameter(Parse(source, SortField.Parse));
+    }
+
+    public static SortQueryParameter Single(SortField field)
+    {
+        return new SortQueryParameter(new([field]));
+    }
+}
+
+public record SortField(string FieldName, bool Ascending)
+{
+    public static SortField Descending(string field) => new(field, false);
+
+    public static SortField Parse(string source)
+    {
+        return source.StartsWith('-') ?
+            new SortField(source[1..], false) 
+            : new SortField(source, true);
+    }
+
+    public override string ToString()
+    {
+        return (Ascending ? "" : "-") + FieldName;
+    }
+}
