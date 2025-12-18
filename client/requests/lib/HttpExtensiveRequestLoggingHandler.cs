95c080b415d8de85f9afe4c6ac4b7c4273feedb8
Egor Tarasov <vosarat1995@google.com>
2025-12-18 13:02:10 +0100

    Nist.Requests: Inited

---
+++ client/requests/lib/HttpExtensiveRequestLoggingHandler.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Nist;
+
+public class HttpExtensiveRequestLoggingHandler<T> : DelegatingHandler
+{
+    private readonly ILogger _logger;
+
+    public HttpExtensiveRequestLoggingHandler(ILoggerFactory logger)
+    {
+        var categoryName = "Nist.HttpExtensiveRequestLoggingHandler." + typeof(T).Name;
+        _logger = logger.CreateLogger(categoryName);
+    }
+
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        string content = "";
+        if (request.Content != null)
+        {
+            content = await request.Content.ReadAsStringAsync(CancellationToken.None);
+        }
+
+        _logger.LogInformation("Sending request: {Method} {Uri} {Content}", request.Method, request.RequestUri, content);
+        var response = await base.SendAsync(request, cancellationToken);
+        return response;
+    }
+}
+
+public static class HttpClientBuilderExtensions
+{
+    public static IHttpClientBuilder WithExtensiveRequestLoggingHandler<T>(this IHttpClientBuilder builder)
+    {
+        builder.Services.AddTransient(typeof(HttpExtensiveRequestLoggingHandler<>));
+        return builder.AddHttpMessageHandler<HttpExtensiveRequestLoggingHandler<T>>();
+    }
+}
+
+
